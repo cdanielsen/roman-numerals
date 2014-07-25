4e3a06b096cc9e64e086b165e755e62060ddf91f
Diane Douglas & Christian Danielsen <student@epicodus.com>
2014-07-25 16:58:40 -0700

    Add a function for double digit numbers, call it from roman_numerals master function, and test it from both functions.

--- lib/roman-numerals.rb
+++ lib/roman-numerals.rb
@@ -13,30 +13,59 @@ def roman_numerals(arabic_num)
   arabic_num = arabic_num.to_s.split("").reverse
 
   the_ones = arabic_num[0].to_i
+  the_tens = arabic_num[1].to_i
   roman_out = single_digits(the_ones)
+  roman_out += double_digits(the_tens)
   # other functions here for larger digits
   roman_out
 end
 
-def single_digits(single_num)
-  the_ones_output = ""
+def double_digits(tens)
+  the_tens_output = ""
   isAdditive = true
-  if single_num == 4 || single_num == 9
+  if tens == 4 || tens == 9
     isAdditive = false
   end
 
     if(isAdditive)
-      if single_num >= 5
-        single_num -=5
+      #binding.pry
+      if tens >= 5
+        tens -=5
+        the_tens_output += 'L'
+      end
+        1.upto(tens) do
+          the_tens_output+= 'X'
+        end
+
+    #subtractive
+    else
+      if tens == 4
+        the_tens_output += 'XL'
+      else the_tens_output += 'XC'
+      end
+    end
+  the_tens_output
+end
+
+def single_digits(ones)
+  the_ones_output = ""
+  isAdditive = true
+  if ones == 4 || ones == 9
+    isAdditive = false
+  end
+
+    if(isAdditive)
+      if ones >= 5
+        ones -=5
         the_ones_output += 'V'
       end
-        1.upto(single_num) do
+        1.upto(ones) do
           the_ones_output+= 'I'
         end
 
     #subtractive
     else
-      if single_num == 4
+      if ones == 4
         the_ones_output += 'IV'
       else the_ones_output += 'IX'
       end
--- spec/roman-numerals_spec.rb
+++ spec/roman-numerals_spec.rb
@@ -14,10 +14,26 @@ describe('roman_numerals') do
   it('converts 4 into a roman numeral') do
     roman_numerals(4).should(eq("IV"))
   end
+
+  it('converts 10 into a roman numeral') do
+    roman_numerals(10).should(eq("X"))
+  end
 end
 
 describe('single_digits') do
   it('takes a single digit and converts it into a roman numeral') do
     single_digits(4).should(eq('IV'))
+  end
+end
+
+describe('double_digits') do
+  it('takes a double digit and converts it into a roman numeral') do
+    double_digits(1).should(eq('X'))
+  end
+  it('takes a double digit and converts it into a roman numeral') do
+    double_digits(3).should(eq('XXX'))
+  end
+  it('takes a double digit and converts it into a roman numeral') do
+    double_digits(4).should(eq('XL'))
   end
 end
