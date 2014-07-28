25be545c4d459563c7fdcbe82c2c6a69ca9825eb
Diane Douglas & Christian Danielsen <student@epicodus.com>
2014-07-28 11:25:42 -0700

    Refactor find_each_digit variable names to make more readable

--- lib/roman-numerals.rb
+++ lib/roman-numerals.rb
@@ -8,27 +8,28 @@ def roman_numerals(arabic_num)
   roman_out
 end
 
-def find_each_digit(digit, placeholder)
+def find_each_digit(digit, placevalue)
   numeral_table = ['I', 'V', 'X', 'L', 'C', 'D', 'M']
   output = ""
-  isAdditive = true
-  if digit == 4 || digit == 9
-    isAdditive = false
-  end
+  isAdditive = !(digit == 4 || digit == 9)
+  degree_of_ten = numeral_table[placevalue * 2]
+  degree_of_five = numeral_table[(placevalue * 2) + 1]
+  next_degree_of_ten = numeral_table[(placevalue * 2) + 2]
 
   if(isAdditive)
     if digit >= 5
-      digit -=5
-      output += numeral_table[placeholder*2 + 1]
+      digit -= 5
+      output += degree_of_five
     end
     1.upto(digit) do
-      output+= numeral_table[placeholder*2]
+      output += degree_of_ten
     end
 
   else
     if digit == 4
-      output += (numeral_table[placeholder*2] + numeral_table[placeholder*2 + 1])
-    else output += (numeral_table[placeholder*2] + numeral_table[placeholder*2 + 2])
+      output += (degree_of_ten + degree_of_five)
+    else
+      output += (degree_of_ten + next_degree_of_ten)
     end
   end
   output
